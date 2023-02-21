5e43d5b3c40c7205df856c26b7f3c42b31d922d5
kboberg98 <kristian.boberg@no.experis.com>
2023-02-21 19:15:43 +0100

    Add project files.

---
+++ SQLClientProject/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLClientProject.Repositories
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "DESKTOP-5AU9VHR\\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity = true;
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
