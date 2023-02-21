d8009956beb5efee75f6112611e733275ad814f5
kboberg98 <kristian.boberg@no.experis.com>
2023-02-21 20:06:15 +0100

    feat: inteface for customer, and tests

--- SQLClientProject/Program.cs
+++ SQLClientProject/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SQLClientProject
+﻿using SQLClientProject.Models;
+using SQLClientProject.Repositories;
+
+namespace SQLClientProject
 {
     internal class Program
     {
@@ -6,5 +9,56 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static void TestSelectAll(ICustomerRepository repository)
+        {
+            PrintCustomers(repository.GetAllCustomers());
+        }
+
+        static void TestSelect(ICustomerRepository repository)
+        {
+            PrintCustomer(repository.GetCustomer("ZZZZ"));
+        }
+
+        static void TestInsert(ICustomerRepository repository)
+        {
+            Customer test = new Customer()
+            {
+                CustomerId = 111,
+                FirstName = "testfirstname",
+                LastName = "testlastname",
+                Country = "norway",
+                PostalCode = "1088",
+                Phone = "24342432", 
+                Email = "testemail@hotmail.com" 
+            };
+            if (repository.AddNewCustomer(test))
+            {
+                Console.WriteLine("YAY, Insert worked");
+            } else
+            {
+                Console.WriteLine("BOO, Insert didnt work");
+            }
+        }
+
+        static void TestDelete(ICustomerRepository repository)
+        {
+            
+        }
+
+        static void PrintCustomers(IEnumerable<Customer> customers)
+        {
+            foreach(Customer customer in customers)
+            {
+                PrintCustomer(customer);
+            }
+        }
+
+        static void PrintCustomer(Customer customer)
+        {
+            Console.WriteLine($"--- {customer.CustomerId} {customer.FirstName} {customer.LastName} {customer.Country} {customer.PostalCode} {customer.Email} {customer.Phone} ---");
+        }
+
+
     }
 }
---
+++ SQLClientProject/Repositories/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using SQLClientProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLClientProject.Repositories
+{
+    public interface ICustomerRepository
+    {
+        public Customer GetCustomer(string id);
+        public List<Customer> GetAllCustomers();
+        public bool AddNewCustomer(Customer customer);
+        public bool UpdateCustomer(Customer customer);
+        public bool DeleteCustomer(string id);
+    }
+}
